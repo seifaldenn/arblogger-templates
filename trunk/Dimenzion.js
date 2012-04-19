80846da0e95742e0f053d9fd73ffd4939117e6e5
ar.blogger.templates1@gmail.com <ar.blogger.templates1@gmail.com@84c22ab2-f71e-331d-ada1-89f693a8e704>
2012-04-19 11:13:00 +0000

    dimenzion blogger theme

---
+++ trunk/Dimenzion.js
@@ -0,0 +1,3 @@
+document.write("<div id='credits_right'>");
+document.write("تصميم <a href='http:\/\/www.besttheme.net\/' title='Blogger Template'>بست ثيم<\/a> - تعريب <a href='http:\/\/www.arblogger-templates.com\/' title='قوالب بلوجر عربية ومجانية'>قوالب بلوجر عربية ومجانية<\/a>");
+document.write("<\/div>");
