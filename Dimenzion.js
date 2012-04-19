9fb9d9fb0286314fb96844b212b8cd4d70830e94
ar.blogger.templates1 <ar.blogger.templates1@gmail.com>
2012-04-19 11:02:56 +0000

    dimenzion template

---
+++ Dimenzion.js
@@ -0,0 +1,3 @@
+document.write("<div id='credits_right'>");
+document.write("Dimenzion <a href='http:\/\/www.besttheme.net\/' title='Blogger Template'>Blogger template<\/a> by <a href='http:\/\/www.besttheme.net\/' title='Best Blogger Templates'>Best Theme<\/a>");
+document.write("<\/div>");
